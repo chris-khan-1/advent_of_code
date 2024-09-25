78292982c7a565d56eaa3da5e2c26f65690979c4
chris_khan <ckhan46.ck@gmail.com>
2024-09-25 21:15:42 +0100

    fix: 2015 day 4 solution

--- 2015/4/day_4_solution.py
+++ 2015/4/day_4_solution.py
@@ -1,7 +1,5 @@
 import hashlib
 
-from utils.variables import Variables
-
 
 def get_md5_hash(secret_key: str, value: int) -> str:
     message = secret_key + str(value)
@@ -28,20 +26,19 @@ def get_lowest_value_required(secret_key: str, digit_length: int) -> int:
 
 
 if __name__ == "__main__":
-    print(Variables.year, Variables.day)
     with open("./2015/4/input.txt", "r") as input_file:
         secret_key = input_file.read()
-        # lowest_value_5_zeroes = get_lowest_value_required(
-        #     secret_key=secret_key, digit_length=5
-        # )
-        # lowest_value_6_zeroes = get_lowest_value_required(
-        #     secret_key=secret_key, digit_length=6
-        # )
+        lowest_value_5_zeroes = get_lowest_value_required(
+            secret_key=secret_key, digit_length=5
+        )
+        lowest_value_6_zeroes = get_lowest_value_required(
+            secret_key=secret_key, digit_length=6
+        )
 
-    # with open("./2015/4/solution.txt", "w") as output_file:
-    #     output_file.write(
-    #         f"Part 1: Lowest value for 5 zeroes is {lowest_value_5_zeroes}\n"
-    #     )
-    #     output_file.write(
-    #         f"Part 2: Lowest value for 6 zeroes is {lowest_value_6_zeroes}\n"
-    #     )
+    with open("./2015/4/solution.txt", "w") as output_file:
+        output_file.write(
+            f"Part 1: Lowest value for 5 zeroes is {lowest_value_5_zeroes}\n"
+        )
+        output_file.write(
+            f"Part 2: Lowest value for 6 zeroes is {lowest_value_6_zeroes}\n"
+        )
