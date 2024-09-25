6620b33e11f414b70e171f3ec94c2ba3cdb5ca5d
chris_khan <ckhan46.ck@gmail.com>
2024-09-25 21:40:29 +0100

    update solution files

---
+++ 2015/8/day_8_solution.py
@@ -0,0 +1,11 @@
+
+
+if __name__ == "__main__":
+    with open("./2015/8/input.txt", "r") as input_file:
+        pass
+
+    with open("./2015/8/solution.txt", "w") as output_file:
+        output_file.write(
+            f"Part 1: ")
+        output_file.write(
+            f"Part 2: ")
--- create_folder_structure.py
+++ create_folder_structure.py
@@ -1,15 +1,25 @@
-import os
+# import os
 
-year = 2015
-for i in range(3, 26):
-    directory = f"./{year}/{i}"
-    if not os.path.exists(directory): 
-        os.makedirs(directory)
-    with open(f"{directory}/input.txt", "w") as output_file:
-        output_file.write("")
-    with open(f"{directory}/instructions.txt", "w") as output_file:
-        output_file.write("")
-    with open(f"{directory}/day_{i}_solution.py", "w") as output_file:
-        output_file.write("") 
-    with open(f"{directory}/test_day_{i}_solution.py", "w") as output_file:
-        output_file.write("")
+# year = 2015
+# for day in range(5, 26):
+#     directory = f"./{year}/{day}"
+#     if not os.path.exists(directory):
+#         os.makedirs(directory)
+#     with open(f"{directory}/input.txt", "w") as output_file:
+#         output_file.write("")
+#     with open(f"{directory}/instructions.txt", "w") as output_file:
+#         output_file.write("")
+#     with open(f"{directory}/day_{day}_solution.py", "w") as output_file:
+#         output_file.write(
+#             f"""\n\nif __name__ == "__main__":
+#     with open("{directory}/input.txt", "r") as input_file:
+#         pass
+
+#     with open("{directory}/solution.txt", "w") as output_file:
+#         output_file.write(
+#             f"Part 1: ")
+#         output_file.write(
+#             f"Part 2: ")"""
+#         )
+#     with open(f"{directory}/test_day_{day}_solution.py", "w") as output_file:
+#         output_file.write("")
