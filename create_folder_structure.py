5e929fec326517d91c649ec9b791840f07a87a15
chris_khan <ckhan46.ck@gmail.com>
2024-09-25 11:31:26 +0100

    add solution files for all days

--- create_folder_structure.py
+++ create_folder_structure.py
@@ -1,11 +1,15 @@
 import os
 
 year = 2015
-for i in range(4, 26):
+for i in range(3, 26):
     directory = f"./{year}/{i}"
     if not os.path.exists(directory):
         os.makedirs(directory)
     with open(f"{directory}/input.txt", "w") as output_file:
         output_file.write("")
     with open(f"{directory}/instructions.txt", "w") as output_file:
+        output_file.write("")
+    with open(f"{directory}/solution.py", "w") as output_file:
+        output_file.write("")
+    with open(f"{directory}/test_solution.py", "w") as output_file:
         output_file.write("")
