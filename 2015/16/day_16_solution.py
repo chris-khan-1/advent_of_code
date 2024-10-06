4dbe62ebdd922b612a1f3d10dc1c9a97ade02f8f
chris_khan <ckhan46.ck@gmail.com>
2024-10-06 14:20:08 +0100

    day 16 solution

--- 2015/16/day_16_solution.py
+++ 2015/16/day_16_solution.py
@@ -1,7 +1,67 @@
+from collections import defaultdict
+
+default_attributes = [
+    "children",
+    "cats",
+    "samoyeds",
+    "pomeranians",
+    "akitas",
+    "vizslas",
+    "goldfish",
+    "trees",
+    "cars",
+    "perfumes",
+]
+
+
+def parse_sue_info(input: list[str]) -> dict[dict]:
+    sue_info = defaultdict(dict)
+    for info_str in input:
+        sue_number_str, attribute_str = info_str.split(sep=": ", maxsplit=1)
+        sue_number = sue_number_str.split(" ")[1]
+        attribute_dict = {i: None for i in default_attributes}
+        for attribute in attribute_str.split(", "):
+            key, value = attribute.split(": ")
+            attribute_dict[key] = int(value)
+        sue_info[sue_number] = attribute_dict
+
+    return sue_info
+
+
+def check_sue(individual_sue_info: dict, test_results: dict):
+    for attribute, value in individual_sue_info.items():
+        if value is not None:
+            if test_results[attribute] != value:
+                return False
+    return True
+
+
+def get_correct_sue(input: list[str], test_results: dict):
+    sue_info = parse_sue_info(input=input)
+    for sue, attributes in sue_info.items():
+        if check_sue(individual_sue_info=attributes, test_results=test_results):
+            return sue
+
+
 if __name__ == "__main__":
     with open("./2015/16/input.txt", "r") as input_file:
-        pass
+        input = [line.strip() for line in input_file.readlines()]
+        correct_sue = get_correct_sue(
+            input=input,
+            test_results={
+                "children": 3,
+                "cats": 7,
+                "samoyeds": 2,
+                "pomeranians": 3,
+                "akitas": 0,
+                "vizslas": 0,
+                "goldfish": 5,
+                "trees": 3,
+                "cars": 2,
+                "perfumes": 1,
+            },
+        )
 
     with open("./2015/16/solution.txt", "w") as output_file:
-        output_file.write(f"Part 1: ")
+        output_file.write(f"Part 1: The correct sue is number {correct_sue}.\n")
         output_file.write(f"Part 2: ")
---
+++ 2015/16/test_day_16_solution.py
@@ -0,0 +1,119 @@
+from day_16_solution import check_sue, get_correct_sue, parse_sue_info
+
+
+def test_parse_sue_info():
+    expected = {
+        "1": {
+            "children": None,
+            "cats": None,
+            "samoyeds": None,
+            "pomeranians": None,
+            "akitas": 0,
+            "vizslas": None,
+            "goldfish": 6,
+            "trees": 9,
+            "cars": None,
+            "perfumes": None,
+        },
+        "3": {
+            "children": None,
+            "cats": None,
+            "samoyeds": None,
+            "pomeranians": None,
+            "akitas": 6,
+            "vizslas": None,
+            "goldfish": None,
+            "trees": None,
+            "cars": 10,
+            "perfumes": 7,
+        },
+    }
+    actual = parse_sue_info(
+        input=[
+            "Sue 1: goldfish: 6, trees: 9, akitas: 0",
+            "Sue 3: cars: 10, akitas: 6, perfumes: 7",
+        ]
+    )
+    assert actual == expected
+
+
+def test_check_sue_success():
+    actual = check_sue(
+        individual_sue_info={
+            "children": None,
+            "cats": None,
+            "samoyeds": None,
+            "pomeranians": None,
+            "akitas": 0,
+            "vizslas": None,
+            "goldfish": 6,
+            "trees": 9,
+            "cars": None,
+            "perfumes": None,
+        },
+        test_results={
+            "children": 1,
+            "cats": 2,
+            "samoyeds": 3,
+            "pomeranians": 4,
+            "akitas": 0,
+            "vizslas": 5,
+            "goldfish": 6,
+            "trees": 9,
+            "cars": 4,
+            "perfumes": 2,
+        },
+    )
+    assert actual is True
+
+
+def test_check_sue_not_her():
+    # akitas don't match
+    actual = check_sue(
+        individual_sue_info={
+            "children": None,
+            "cats": None,
+            "samoyeds": None,
+            "pomeranians": None,
+            "akitas": 5,
+            "vizslas": None,
+            "goldfish": 6,
+            "trees": 9,
+            "cars": None,
+            "perfumes": None,
+        },
+        test_results={
+            "children": 1,
+            "cats": 2,
+            "samoyeds": 3,
+            "pomeranians": 4,
+            "akitas": 0,
+            "vizslas": 5,
+            "goldfish": 6,
+            "trees": 9,
+            "cars": 4,
+            "perfumes": 2,
+        },
+    )
+    assert actual is False
+
+
+def test_get_correct_sue():
+    input = [
+        "Sue 1: goldfish: 6, trees: 9, akitas: 0",
+        "Sue 3: cars: 10, akitas: 6, perfumes: 7",
+    ]
+    test_results = {
+        "children": 1,
+        "cats": 2,
+        "samoyeds": 3,
+        "pomeranians": 4,
+        "akitas": 6,
+        "vizslas": 5,
+        "goldfish": 6,
+        "trees": 9,
+        "cars": 10,
+        "perfumes": 7,
+    }
+    actual = get_correct_sue(input=input, test_results=test_results)
+    assert actual == "3"
